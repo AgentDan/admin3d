3fb8b09dc19c5afe848dad864fd2a3c808d3b60f
AgentDan <bion-1@mail.ru>
2025-03-16 00:38:29 +0100

    commit

--- index.js
+++ index.js
@@ -14,4 +14,15 @@ app.use(express.json({extended: true}))
 app.use('/api/file', require('./routes/file'))
 app.use('/api/user', require('./routes/user'))
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, './client/dist')));
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, './client/dist/index.html')
+        )
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
+
 app.listen(PORT, ()=> console.log("Start server : ", PORT))
